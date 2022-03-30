00a096cbb49a92d2d4f660d87f7e82ab091f3557
Wemi Moyela <wemymoyela@gmail.com>
2022-03-30 20:08:25 +0100

    event handling

--- part01/src/App.js
+++ part01/src/App.js
@@ -3,13 +3,45 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
 
+  // console.log('rendering...', counter)
+
+  const increase = () => {
+  
+    setCounter(counter + 1)
+    console.log('... plus one equals ... ' + counter)
+  }
+
+  const reduce = () => {
+
+    setCounter(counter - 1)
+    console.log('... when one is removed we have ... ' + counter)
+  }
+
+  const reset = () => {
+
+    setCounter(0)
+    console.log('value is reset to zero')
+  }
   return (
-    <div>{counter}</div>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={increase}>
+        Increase
+      </button>
+      <button onClick={reduce}>
+        Decrease
+      </button>
+      <div>
+      <button onClick={reset} >
+        RESET
+      </button>
+      </div>
+    </>
   )
 }
 
