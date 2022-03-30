d88cbc9cc4302f5c3b53f5fd9d6b7bf96167bb2a
Wemi Moyela <wemymoyela@gmail.com>
2022-03-30 20:38:50 +0100

    passing state to child components

--- part01/src/App.js
+++ part01/src/App.js
@@ -3,13 +3,6 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  // setTimeout(
-  //   () => setCounter(counter + 1),
-  //   1000
-  // )
-
-  // console.log('rendering...', counter)
-
   const increase = () => {
   
     setCounter(counter + 1)
@@ -29,20 +22,45 @@ const App = () => {
   }
   return (
     <>
-      <h1>{counter}</h1>
-      <button onClick={increase}>
-        Increase
-      </button>
-      <button onClick={reduce}>
-        Decrease
-      </button>
-      <div>
-      <button onClick={reset} >
-        RESET
-      </button>
-      </div>
+      <Display counter={counter} />
+      <IncreaseButton onClick={increase} />
+      <DecreaseButton onClick={reduce} />
+      <ResetButton onClick={reset} />
     </>
   )
 }
+
+const Display = (props) => {
+  return (
+    <h1>{props.counter}</h1>
+  )
+}
+
+const IncreaseButton = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      Increase
+    </button>
+  )
+}
+
+const DecreaseButton = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      Decrease
+    </button>
+  )
+}
+
+const ResetButton = (props) => {
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        RESET
+      </button>
+    </div>
+  )
+}
+
 
 export default App
