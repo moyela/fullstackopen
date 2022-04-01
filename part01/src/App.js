1851a9d2a75f92033580f49d4eae2d0284f06d61
Wemi Moyela <wemymoyela@gmail.com>
2022-04-01 15:39:08 +0100

    what's wrong? I have no idea

--- part01/src/App.js
+++ part01/src/App.js
@@ -1,89 +1,85 @@
 import { useState } from 'react'
 
-// unicafe example application 
+// anecdotes example application 
 // for the University of Helsinki's fullstackopen 2022 program
 
 const App = () => {
 
-  //state saves clicks of each feedback to the component state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    '',
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
 
-  let all = good + neutral + bad
-  let average = (good - bad) / 3
-  let positivePercent = ((good / all) * 100) + ' %'
- 
+  //storing state to pick anecdote based on index key
+  const [selected, setSelected] = useState(0)
 
-  const submitGood = () => setGood(good + 1)
-  const submitNeutral = () => setNeutral(neutral + 1)
-  const submitBad = () => setBad(bad + 1)
-
-  return (
-    <>
-      <Sections sectionTitle='Give Feedback'/>
-
-      <FeedbackButton text='Good' handleClick={submitGood} />
-      <FeedbackButton text='Neutral' handleClick={submitNeutral} />
-      <FeedbackButton text='Bad' handleClick={submitBad} />
-
-      <Statistics good={good} bad={bad} neutral={neutral} all={all} average={average} positive={positivePercent}/>
-
-    </>
-  )
-}
-
-const Statistics = (props) => {
-
-  if (props.all === 0) {
-    return (
-      <>
-        <Sections sectionTitle='Statistics' />
-        <div>NO FEEDBACK GIVEN. CLICK ABOVE TO GIVE FEEDBACK</div>
-      </>
-    )
+  //function to generate a random integer to set state for the anecdote index key
+  const randomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
   }
 
+  //function to execute the random integer generation and randomize the anecdote state
+  const newAnecdote = () => {
+    setSelected(randomInt(1, 7))
+    console.log('Anecdote', selected, 'has been selected')
+    return selected
+  }
+
+  let votes = [0, 0, 0, 0, 0, 0, 0, 0]
+
+  const voteForQuote = () => {
+    whichAnecdote += 1
+    console.log('Anecdote', selected, 'has been voted for', whichAnecdote, 'time(s)')
+    return whichAnecdote
+  }
+
+  let whichAnecdote = votes[selected]
+
   return (
     <>
-      <Sections sectionTitle='Statistics' />
-
-      <table>
-        <tbody>
-          <StatisticLine title='Good' figure={props.good} />
-          <StatisticLine title='Neutral' figure={props.neutral} />
-          <StatisticLine title='Bad' figure={props.bad} />
-          <StatisticLine title='Total Feedback' figure={props.all} />
-          <StatisticLine title='Average' figure={props.average} />
-          <StatisticLine title='Proportion of Positive votes' figure={props.positive} />
-        </tbody>
-      </table>
+      <Anecdotes anecdotes={anecdotes} selected={selected}/>
+      <VoteCount number={voteForQuote()}  selected={selected} />
+      <Button text='Click here to generate a random anecdote'
+      handleClick={newAnecdote}/>
+      <Button text='Vote' handleClick={voteForQuote}/>
     </>
   )
 }
 
-const Sections = ({sectionTitle}) => {
+const Anecdotes = ({anecdotes, selected}) => {
   return (
-    <>
-      <h1>{sectionTitle}</h1>
-    </>
+    <h3>
+      {anecdotes[selected]}
+    </h3>
   )
 }
 
-const FeedbackButton = ({text, handleClick}) => {
+const Button = ({text, handleClick}) => {
   return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
+    <p>
+      <button onClick={handleClick}>
+        {text}
+      </button>
+    </p>
   )
 }
 
-const StatisticLine = ({title, figure}) => {
+const VoteCount = ({selected, number}) => {
+  if (selected > 0) {
+    return (
+      <div>
+        This quote has {number} votes
+      </div>
+    )
+  }
   return (
-    <tr>
-      <td>{title}</td>
-      <td>{figure}</td>
-    </tr>
+    <></>
   )
 }
 
