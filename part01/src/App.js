27259076348e2c371fb16a47f882eb7ac02783e3
Wemi Moyela <wemymoyela@gmail.com>
2022-04-08 12:38:58 +0100

    exercise 1.14

--- part01/src/App.js
+++ part01/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const anecdotes = [
     '',
     'If it hurts, do it more often',
+    'If not for the big bang, shey I go dey this side? Oluwa, HANDLE IT OH!',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
@@ -19,44 +20,60 @@ const App = () => {
   //storing state to pick anecdote based on index key
   const [selected, setSelected] = useState(0)
 
+  // votes stores the number of times a vote has been made for each anecdote position
+  const [votes, setVotes] = useState([0,0,0,0,0,0,0,0,0])
+  
+  // clicks stores the number of times a vote has been made, it is increased by voteForQuote
+  const [clicks, setClicks] = useState(0)
+
   //function to generate a random integer to set state for the anecdote index key
   const randomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min
   }
 
   //function to execute the random integer generation and randomize the anecdote state
+  
   const newAnecdote = () => {
-    setSelected(randomInt(1, 7))
-    console.log('Anecdote', selected, 'has been selected')
+    setSelected(randomInt(1, 8))
+    console.log('newAnecdote(): Anecdote', selected, 'has been selected')
     return selected
   }
 
-  let votes = [0, 0, 0, 0, 0, 0, 0, 0]
+  // whichAnecdote specifies the anecdote by its position in the array 'anecdotes'
+  let whichAnecdote = votes[selected]
 
   const voteForQuote = () => {
-    whichAnecdote += 1
-    console.log('Anecdote', selected, 'has been voted for', whichAnecdote, 'time(s)')
-    return whichAnecdote
+    let counter = clicks
+    setClicks(counter + 1)
+    let quoteCounter = [...votes]
+    quoteCounter[selected] += 1
+    setVotes(quoteCounter)
+    console.log('voteForQuote(): Anecdote', selected, 'has been voted for', quoteCounter[selected], 'time(s)')
+    return quoteCounter[selected]
   }
-
-  let whichAnecdote = votes[selected]
 
   return (
     <>
       <Anecdotes anecdotes={anecdotes} selected={selected}/>
-      <VoteCount number={voteForQuote()}  selected={selected} />
+      <VoteCount number={whichAnecdote}  selected={selected} />
       <Button text='Click here to generate a random anecdote'
       handleClick={newAnecdote}/>
       <Button text='Vote' handleClick={voteForQuote}/>
+      <MostVotes clicks={clicks} anecdotes={anecdotes} selected={selected}/>
     </>
   )
 }
 
 const Anecdotes = ({anecdotes, selected}) => {
   return (
-    <h3>
-      {anecdotes[selected]}
-    </h3>
+    <>  
+      <h2>Anecdote of the day</h2>
+      <div>
+        <i>
+          {anecdotes[selected]}
+        </i>
+      </div>
+    </>
   )
 }
 
@@ -73,14 +90,36 @@ const Button = ({text, handleClick}) => {
 const VoteCount = ({selected, number}) => {
   if (selected > 0) {
     return (
-      <div>
-        This quote has {number} votes
-      </div>
+      <p>
+        This quote has <strong>{number}</strong> votes
+      </p>
     )
   }
   return (
     <></>
   )
+}
+
+const MostVotes = ({clicks, anecdotes, selected}) => {
+  if (clicks > 0) {
+    return (
+      <>
+        <h2>Anecdote with the most votes</h2>
+        <div>
+        <i>
+          {anecdotes[selected]}
+        </i>
+      </div>
+      </>
+    )
+  }
+
+  return (
+    <h5>
+      Vote for a quote to show the anecdote with the most quotes
+    </h5>
+  )
+
 }
 
 export default App
