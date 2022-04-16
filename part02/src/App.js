f7cae62e9bcff5983f8737fe1ce02ab07f5783d9
Wemi Moyela <wemymoyela@gmail.com>
2022-04-16 12:49:05 +0100

    exercises 2.1 - 2.5

--- part02/src/App.js
+++ part02/src/App.js
@@ -1,15 +1,66 @@
-const App = (props) => {
-  const { notes } = props
+const App = () => {
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 50,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
+      <Course course={course} />
     </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header course={course}/>
+      <Content course={course}/>
+    </>
+
+  )
+}
+
+const Header = ({course}) => {
+  console.log(course)
+  return (
+  <>
+    <h1>{course.name}</h1>
+  </>)
+}
+
+const Content = ({course}) => {
+  return (
+    <>
+      <Part course={course}/>
+    </>
+  )
+}
+
+const Part = ({course}) => {
+  console.log(course.parts.name, course.parts.exercises)
+  return (
+  <>
+    {course.map( course => <p>{course.parts.name} {course.parts.exercises}</p>)}
+  </>
   )
 }
 
