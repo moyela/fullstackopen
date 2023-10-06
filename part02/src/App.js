f56e86058d9178b5a424636fd19965780303747f
Wemi Moyela <wemymoyela@gmail.com>
2023-10-06 15:01:33 +0100

    updated module 2 file system

--- part02/src/App.js
+++ part02/src/App.js
@@ -1,77 +1,15 @@
-const App = () => {
+const App = (props) => {
+  const { notes } = props
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 50,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Testing servers in React',
-        exercises: 22,
-        id: 4
-      }
-    ]
-  }
-
-  console.log('APP WORKS...')
   return (
     <div>
-      <Course course={course} />
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
     </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course} />
-    </>
-
-  )
-}
-
-const Header = ({course}) => {
-  console.log(course)
-  return (
-  <>
-    <h1>{course.name}</h1>
-  </>)
-}
-
-const Content = ({course}) => {
-  return (
-    <>
-      {course.parts.map(object => <p key={object.id}>{object.name} {object.exercises}</p>)}
-    </>
-  )
-}
-
-const Total = ({course}) => {
-
-  let totalArray = course.parts.map(object => object.exercises)
-
-  const sumOfArray = (first, remainder) => first + remainder
-  let total = totalArray.reduce(sumOfArray)
-  return (
-    <h4>
-      Total number of exercises is {total}
-    </h4>
   )
 }
 
--- part02/src/index.js
+++ part02/src/index.js
@@ -1,7 +1,27 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
 
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
+
 ReactDOM.render(
-  <App />, 
+  <App notes={notes} />, 
   document.getElementById('root')
 )
